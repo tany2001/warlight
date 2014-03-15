35155fd5e5f9e2cf5566c425a4a69f6c7c0bda1d
tany2001 <tany2001@gmail.com>
2014-03-15 15:58:17 +0200

    added Game class

--- client.js
+++ client.js
@@ -2,7 +2,26 @@ var socket = io.connect("http://localhost");
 
 var nickname;
 while(nickname == undefined || nickname == "")
-	nickname = prompt("Enter your username", "Sasho");
+
+socket.emit("login", {name: nickname});
+
+var Game = {};
+Game.regionList = [];
+Game.players = [];
+Game.Player = function ()
+{
+	this.name = "";
+	this.orders = [];
+	this.id = "";
+}
+Game.Region = function (px, py)
+{
+	this.neighbors = [];
+	this.pos = {x: px, y: py};
+	this.size = {x: 10, y: 10};
+	this.owner = "";
+	this.id = "";
+}
 
 socket.on("news", function (data)
 {
