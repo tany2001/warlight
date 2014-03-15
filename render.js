a1ec13e7123dc3cbb5f88fecd48d9192be7365fb
NikiRann <NikiRann@gmail.com>
2014-03-15 17:34:29 +0200

    UI begining + resizing

--- render.js
+++ render.js
@@ -1,4 +1,6 @@
 var canvas = document.getElementById("mainCanvas");
+var Deployimage = document.getElementById("Deploy");
 var context = canvas.getContext("2d");
-
+canvas.width = screen.width - 10;
+canvas.height = screen.height - Deployimage.height - 200;
 context.strokeRect(0, 0, canvas.width, canvas.height);
