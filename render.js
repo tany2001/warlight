98b0620144783960eac43cd1a1e270cdeafde638
NikiRann <NikiRann@gmail.com>
2014-03-16 11:43:27 +0200

    abe napulno se osrah nai nakrq go opraih, nadqvam se da raboti

--- render.js
+++ render.js
@@ -7,42 +7,41 @@ canvas.height = screen.height - 200;
 context.strokeRect(0, 0, canvas.width, canvas.height);
 
 
-game.mouse = {};
-game.selectedRegionId = -1;
-
-game.Render.DrawRegion = function DrawRegion(current){
+Game.mouse = {};
+Game.selectedRegionId = -1;
+Game.Render.DrawRegion = function DrawRegion(current){
 	
 	context.globalAlpha = 1;
 	
-	if (game.myself.id == current.owner) {
+	if (Game.myself.id == current.owner) {
 		context.fillStyle = "green";
 	}else {
 		context.fillStyle = "red";
 	}
-	if (current.id == game.selectedRegionId) {
+	if (current.id == Game.selectedRegionId) {
 		context.globalAlpha = 0.7;
 	}
 	context.fillRect(current.pos.x,current.pos.y,current.size.x,current.size.y);
 } 
-game.Render.DrawAllRegion = function DrawRegion(){
-	for (var i = 0 ; i < game.regionList.length; i ++) {
-		game.Render.DrawRegion (game.regionList[i]);
+Game.Render.DrawAllRegion = function DrawRegion(){
+	for (var i = 0 ; i < Game.regionList.length; i ++) {
+		Game.Render.DrawRegion (Game.regionList[i]);
 	}
 } 
-game.Render.Draw = function Draw() {
+Game.Render.Draw = function Draw() {
 
 	context.strokeRect(0, 0, canvas.width, canvas.height);
-	game.Render.DrawAllRegion();
+	Game.Render.DrawAllRegion();
 	
 	requestAnimationFrame();
 }
-game.squareCollision = function squareCollision (o1,o2) {
+Game.squareCollision = function squareCollision (o1,o2) {
 	return o1.pos.x + o1.size.x > o2.pos.x && o1.pos.y + o1.size.y > o2.pos.y && o2.pos.x + o2.size.x > o1.pos.x && o2.pos.y + o2.size.y > o1.pos.y;
 }
-game.FindRegionCollided = function FindRegionCollided (mouse){
-	for ( var i = 0 ; i < game.regionList.length ; i ++ ) {
-		if (game.squareCollision ( { pos: {x:mouse.x , y:mouse.y},size: {0,0} } , game.regionList[i] ) ) {
-			return game.regionList[i].id;
+Game.FindRegionCollided = function FindRegionCollided (mouse){
+	for ( var i = 0 ; i < Game.regionList.length ; i ++ ) {
+		if (Game.squareCollision ( { pos: {x:mouse.x , y:mouse.y},size: {0,0} } , Game.regionList[i] ) ) {
+			return Game.regionList[i].id;
 		}
 	}
 	return -1;
@@ -50,13 +49,13 @@ game.FindRegionCollided = function FindRegionCollided (mouse){
 	
 window.addEventListener("mouseup", function (args) {
 	
-	game.mouse.x = args.x;
-	game.mouse.y = args.y;
+	Game.mouse.x = args.x;
+	Game.mouse.y = args.y;
 	
-	game.selectedRegionId = game.FindRegionCollided(game.mouse);
+	Game.selectedRegionId = Game.FindRegionCollided(Game.mouse);
 	
 	
 }, false);
 
 
-game.Render.Draw();
+Game.Render.Draw();
