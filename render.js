d4b1fe2737b850f9c22fe91648c4b98f2540130f
NikiRann <NikiRann@gmail.com>
2014-03-16 17:20:35 +0200

    sasasaas

--- render.js
+++ render.js
@@ -33,6 +33,7 @@ context.strokeRect(0, 0, canvas.width, canvas.height);
  	context.strokeRect(0, 0, canvas.width, canvas.height);
  	Game.Render.DrawAllRegion();
 	
+
  	requestAnimationFrame();
  }
  Game.squareCollision = function squareCollision (o1,o2) {
@@ -40,12 +41,25 @@ context.strokeRect(0, 0, canvas.width, canvas.height);
  }
  Game.FindRegionCollided = function FindRegionCollided (mouse){
  	for ( var i = 0 ; i < Game.regionList.length ; i ++ ) {
- 		if (Game.squareCollision ( { pos: {x:mouse.x , y:mouse.y},size: {0,0} } , Game.regionList[i] ) ) {
+ 		if (Game.squareCollision ( { pos: {x:mouse.x , y:mouse.y},size: {x : 0,y : 0} } , Game.regionList[i] ) ) {
  			return Game.regionList[i].id;
  		}
  	}
  	return -1;
  }
+	requestAnimationFrame();
+}
+Game.squareCollision = function squareCollision (o1,o2) {
+	return o1.pos.x + o1.size.x > o2.pos.x && o1.pos.y + o1.size.y > o2.pos.y && o2.pos.x + o2.size.x > o1.pos.x && o2.pos.y + o2.size.y > o1.pos.y;
+}
+Game.FindRegionCollided = function FindRegionCollided (mouse){
+	for ( var i = 0 ; i < Game.regionList.length ; i ++ ) {
+		if (Game.squareCollision ( { pos: {x:mouse.x , y:mouse.y},size: {x:0,y:0} } , Game.regionList[i] ) ) {
+			return Game.regionList[i].id;
+		}
+	}
+	return -1;
+}
 	
  window.addEventListener("mouseup", function (args) {
 	
