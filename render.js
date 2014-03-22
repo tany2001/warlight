d1a34cef162fe1c2e2fabe470a1ee62af329fc7d
NikiRann <NikiRann@gmail.com>
2014-03-22 17:19:29 +0200

    Second Button

--- render.js
+++ render.js
@@ -1,9 +1,9 @@
 var canvas = document.getElementById("mainCanvas");
-var Deployimage = document.getElementById("Deploy");
+// var Deployimage = document.getElementById("Deploy");
 var context = canvas.getContext("2d");
 
-canvas.width = screen.width - 60 - Deployimage.width;
-canvas.height = screen.height - 200;
+// canvas.width = screen.width - 60 - Deployimage.width;
+// canvas.height = screen.height - 200;
 context.strokeRect(0, 0, canvas.width, canvas.height);
 
 
