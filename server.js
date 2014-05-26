3befb0b8d6d54b0da76bd4a909c7a022325063e9
Stanislav Nikolov <tany2001@gmail.com>
2014-05-26 13:37:38 +0300

    Now order.js, styles.css, Attackbutton.png and the other png CAN be loaded from the server.

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ server.listen(8080);
 app.get("/", function (req, res) { res.sendfile(__dirname + "/index.html"); });
 app.get("/client.js", function (req, res) { res.sendfile(__dirname + "/client.js"); });
 app.get("/render.js", function (req, res) { res.sendfile(__dirname + "/render.js"); });
+app.get("/images/Placebutton.png", function (req, res) { res.sendfile(__dirname + "/images/Placebutton.png"); });
+app.get("/images/Attackbutton.png", function (req, res) { res.sendfile(__dirname + "/images/Attackbutton.png"); });
+app.get("/styles.css", function (req, res) { res.sendfile(__dirname + "/styles.css"); });
+app.get("/order.js", function (req, res) { res.sendfile(__dirname + "/order.js"); });
 
 var users = [];
 
